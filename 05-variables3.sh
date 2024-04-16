1a5d68e3d3517a59e94d304a0be4e51af40c23c5
amhere10 <iamheretolearn10@gmail.com>
2024-04-16 22:05:03 +0530

    Scripting started

--- 05-variables3.sh
+++ 05-variables3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your username"
-read -s USERNAME
+read -s USERNAME  # USERNAME is used as a variable & -s keeps the text in hidden mode
 echo "Please enter your password"
-read -s PASSWORD
+read -s PASSWORD  # PASSWORD is used as a variable & -s keeps the text in hidden mode
 echo "Username is :: $USERNAME, Password is :: $PASSWORD"
---
+++ 06-array0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Movies=("Darling" "Mirchi" "Rebel" "BH1" "BH2")
+
+echo "First Movie name is: ${Movies[0]}
+echo "Second Movie name is: ${Movies[1]}
+echo "Third Movie name is: ${Movies[2]}
+echo "All Movie names are: ${Movies[@]}
