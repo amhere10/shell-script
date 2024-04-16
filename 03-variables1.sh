6e923250e575de2431418b431fc3401e1e9663e2
amhere10 <iamheretolearn10@gmail.com>
2024-04-16 20:41:25 +0530

    Scripting started

---
+++ 03-variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Saharsh
+PERSON2=Sahithi
+
+echo "$PERSON1 :: Namasthey, $PERSON2. How are you?"
+echo "$PERSON2 :: Namasthey $PERSON1. I am fine. What about you?"
+echo "$PERSON1 :: I a good too."
+echo "$PERSON2 :: what is the update regarding Rama-Navami festival"
+echo "$PERSON1 :: As of now everything is going smooth and good"
