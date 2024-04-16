732c71dd0b702d19145d1aad7b981e6b16d55b27
amhere10 <iamheretolearn10@gmail.com>
2024-04-16 20:31:31 +0530

    Scripting started

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is also sample script file.
+#!/bin/bash
+
+echo "Hanuman :: Namasthey, Rama. How are you?"
+echo "Rama :: Namasthey Hanuman. I am fine. What about you?"
+echo "Hanuman :: I a good too."
+echo "Rama :: what is the update regarding Rama-Navami"
