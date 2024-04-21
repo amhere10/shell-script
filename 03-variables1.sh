b218a9abeba695b4c559bd48d6e5f71ac9765c02
amhere10 <iamheretolearn10@gmail.com>
2024-04-21 14:26:16 +0530

    modified

--- 03-variables1.sh
+++ 03-variables1.sh
@@ -7,6 +7,6 @@ PERSON2=Sahithi
 #Using the Variables by referring.
 echo "$PERSON1 :: Namasthey, $PERSON2. How are you?"
 echo "$PERSON2 :: Namasthey $PERSON1. I am fine. What about you?"
-echo "$PERSON1 :: I a good too."
+echo "$PERSON1 :: I am good too."
 echo "$PERSON2 :: what is the update regarding Rama-Navami festival"
 echo "$PERSON1 :: As of now everything is going smooth and good"
